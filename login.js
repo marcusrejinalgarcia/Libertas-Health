2fe7e646f0c1ab0f9d76e8afa5fd5eceb0682b20
Marcus Rejinal Garcia <marcusrejinalgarcia@gmail.com>
2022-03-04 02:33:46 +0800

    initial commit

---
+++ login.js
@@ -0,0 +1,34 @@
+const btnLogin = document.forms["login-form"]["button-login"];
+const notifier = document.getElementById("notifier");
+
+let username = document.forms["login-form"]["username"];
+let password = document.forms["login-form"]["password"];
+
+function checkUsername() {
+    let uName = username.value.trim();
+    if (uName.length < 5 || uName.length > 16) {
+        notifier.innerText += "Username must be between 5 and 16 characters. "
+        return false;
+    } else return true;
+};
+
+function checkPassword() {
+    let pWord = password.value.trim();
+    let pwRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8}$/;
+    if (!pwRegex.test(pWord)) {
+        notifier.innerText += 'Password must be at least 8 characters long, and contain at least 1 digit, 1 uppercase letter, and 1 lowercase letter.';
+        return false;
+    } else return true;
+}
+
+function checkLogin() {
+    if (checkUsername() && checkPassword()) {
+        localStorage.setItem("Username", JSON.stringify(username.value)); // store username in localStorage
+        window.location = "patientInfo.html"; // load next page
+    } 
+}
+
+btnLogin.addEventListener('click', (event) => {
+    event.preventDefault();
+    checkLogin();
+})
---
+++ patientForm.js
@@ -0,0 +1,36 @@
+// show greeting with username
+let greeting = document.getElementById("greeting");
+greeting.innerText = `Welcome, ${JSON.parse(localStorage.getItem("Username"))}!`;
+
+const storePatientInfo = (event) => {
+    let patientInfo = {
+        name: [
+            document.getElementById("firstName").value,
+            document.getElementById("middleName").value,
+            document.getElementById("lastName").value
+        ],
+        address: document.getElementById("address").value,
+        birthdate: document.getElementById("birthdate").value,
+        gender: document.querySelector('input[name="gender"]:checked').value,
+        contactNumber: document.getElementById("contact-number").value,
+        medicalHistory: [],
+        currentSymptoms: [],
+        medication: document.querySelector('input[name=medication]:checked').value
+    }
+
+    let medHistory = document.querySelectorAll('input[name="med-history"]');
+    [...medHistory].forEach(condition => {
+        if (condition.checked) patientInfo.medicalHistory.push(condition.value);
+    });
+
+    let symptomList = document.getElementById("symptoms").getElementsByTagName("option");
+    [...symptomList].forEach(symptom => {
+        if (symptom.selected) patientInfo.currentSymptoms.push(symptom.value);
+    });
+
+    localStorage.setItem("Patient Information", JSON.stringify(patientInfo, null, 2));
+    console.log("Patient information stored successfully.");
+}
+
+let btnSubmit = document.getElementById("btn-submit");
+btnSubmit.addEventListener('click', storePatientInfo);
